7050e36c8b084486c2aa2ad6ffb42cb10a1b9fe5
Mon Lee <lee.557100@gmail.com>
2020-05-18 18:25:09 +0800

    Initial commit

---
+++ app/src/main/java/com/lemon/githubuploadtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lemon.githubuploadtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
